7e9af6c97ae35bddb351d5b047f87b48bb492e51
Zeyd Khalil <zk1789a@student.american.edu>
2020-11-28 18:08:41 +0400

    commit?

--- Untitled.R
+++ Untitled.R
@@ -2,5 +2,6 @@
 library(dplyr)
 
 
-covid_stats <- read.csv('/Users/Yoshimune/Desktop/American University/STAT 412/Project/owid-covid-data.csv')
+covid_stats <- 
+  read.csv('/Users/Yoshimune/Desktop/American University/STAT 412/Project/owid-covid-data.csv')
 covid_stats
